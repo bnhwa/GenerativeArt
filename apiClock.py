e8ac17a1a17671d45362c3f2951d32639383c2e9
Bailey <bb339933@gmail.com>
2021-01-22 20:02:47 -0500

    works now fully clock is synced to show jst

--- apiClock.py
+++ apiClock.py
@@ -10,9 +10,20 @@ api key:   C7BV3SHDW5LP
 import requests
 import time
 import importlib
-# import board
-# import neopixel
-###see if libraries are imported, if not, install them
+import board
+import neopixel
+pixel_pin = board.D21
+
+# The number of NeoPixels
+num_pixels = 8
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
 
 def current_milli_time():
     return round(time.time() * 1000)
@@ -26,7 +37,7 @@ def request(lat,lng):
     if (res['status']=="OK"):
         start = time.time()
         ts = res["formatted"].split(' ')[-1].split(":")
-        print(ts)
+        #print(ts)
         secs = (3600*int(ts[0]))+(60*int(ts[1]))+int(ts[2])
         
         end = time.time()
@@ -40,20 +51,28 @@ def request(lat,lng):
 def main():
     lat = 35.6762
     lng = 139.6503
-    # lat = 51.5074
-    # lng = -0.1278
+    #lat = 51.5074
+    #lng = -0.1278
     totsec = 86400
     ctr = request(lat,lng)
-    print(ctr)
-    print(ctr/86400)
-    
-    
-    #tot seconds = 86400
-    # while(True):
-    #     if (ctr>86400):
-    #         ctr = 0
-    #     pass
-    
+    print("initialized")
+    #print(ctr)
+    #print(ctr/86400)
+    print(len(pixels))
+    cols = [(255,0,0),(25,23,225)]
+    while(True):
+        if (ctr>totsec):
+            ctr = 0
+        #if daytime disp red,
+        #nighttime display blue
+        sel = cols[0]  if (ctr<(totsec/2)) else cols[1]
+
+        for i in range(int(num_pixels*(ctr/totsec))):
+            pixels[i] = sel
+        pixels.show()
+        ctr+=1
+        #pixels.show()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
